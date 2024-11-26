f867ced5d2bba4af946640d9230bdc118df4da0b
“EniSza” <encsi20010101@gmail.com>
2024-11-26 20:22:31 +0100

    első push

---
+++ car.java
@@ -0,0 +1,29 @@
+package src;
+
+public class car {
+	
+	int suly;
+	int ev;
+	
+	String tipus;
+	public String getTipus() {
+		return tipus;
+	}
+	public void setType(String tipus) {
+		this.tipus = tipus;
+	}
+	public int getSuly() {
+		return suly;
+	}
+	public void setSuly(int suly) {
+		this.suly = suly;
+	}
+	public int getev() {
+		return ev;
+	}
+	public void setev(int ev) {
+		this.ev = ev;
+		
+	}
+	
+}
